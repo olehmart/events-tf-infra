09ebdaad0f174123a27a0c91a088d3110979c203
Oleh Martushevskyi <oleh.martushevskyi@telus.com>
2022-06-30 14:55:53 -0400

    infra

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "instrumentation-events" {
-  source     = "git::https://github.com/olehmart/events-tf-module.git?ref=v0.1.0"
+  source     = "git::https://github.com/olehmart/events-tf-module.git?ref=v0.1.1"
   project_id = var.project_id
 }
