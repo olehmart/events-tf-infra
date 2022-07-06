7da4382525cd743eab5d96ae2639714895936c81
Oleh Martushevskyi <oleh.martushevskyi@telus.com>
2022-07-06 11:23:11 -0400

    infra

--- main.tf
+++ main.tf
@@ -3,3 +3,31 @@ module "instrumentation-events" {
   source     = "git::https://github.com/olehmart/events-tf-module.git?ref=main"
   project_id = var.project_id
 }
+
+output "cloud_build_topics" {
+  value = module.instrumentation-events.cloud_build_topics
+}
+
+output "cloud_deploy_topics" {
+  value = module.instrumentation-events.cloud_deploy_topics
+}
+
+output "cloud_build_subscriptions" {
+  value = module.instrumentation-events.cloud_build_subscriptions
+}
+
+output "cloud_deploy_subscriptions" {
+  value = module.instrumentation-events.cloud_deploy_subscriptions
+}
+
+output "webhook_receiver_url" {
+  value = module.instrumentation-events.webhook_receiver_url
+}
+
+output "webhook_cloud_deploy_path" {
+  value = module.instrumentation-events.webhook_cloud_deploy_path
+}
+
+output "webhook_cloud_build_path" {
+  value = module.instrumentation-events.webhook_cloud_build_path
+}
